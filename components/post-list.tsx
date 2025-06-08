3c1ae18aabf3ee768e7c8a97181fcc1cb0d846bc
Jan Raasch <jan@janraasch.com>
2025-06-08 22:07:50 +0200

    Revert "fix: `Image` should not have both `fill` & `width` properties"
    This reverts commit 43530574eb3fa6d4d00509d1e9213628c3e43e4f.

--- components/post-list.tsx
+++ components/post-list.tsx
@@ -251,6 +251,8 @@ export default function PostList({
                           alt={image.file_name}
                           placeholder="blur"
                           fill
+                          width="96"
+                          height="96"
                           blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+P+/HgAEtAJJXIDTjwAAAABJRU5ErkJggg=="
                           style={{ objectFit: "cover" }}
                         />
